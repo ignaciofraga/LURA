e841ed72ce95eb7a6d1c771740c36786bef2a193
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2023-04-24 12:04:09 +0200

    ss

--- recortable.py
+++ recortable.py
@@ -8,10 +8,44 @@ Created on Mon Apr 24 11:13:13 2023
 import streamlit as st
 from PIL import Image
 
-logo_IEO_principal    = 'DATOS/IMAGENES/logo-CSIC.jpg'    
+logo_IEO_principal   = 'DATOS/IMAGENES/ieo.jpg'    
+archivo_recortable   = 'DATOS/PLANO_LURA_A3.pdf'
 
-st.title("Plataforma de datos biogeoquímicos del C.O de A Coruña")
 
-# Añade el logo del IEO
-imagen_pagina = Image.open(logo_IEO_principal) 
-st.image(imagen_pagina)
+st.title("**Monta el barco del Centro Oceanográfico en tu propia casa!**")
+
+st.text('Descarga el recortable pulsando en el botón inferior. Imprime los planos en un A3, monta el barco y comienza a navegar!!')
+        
+    
+with open(archivo_recortable, "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+
+st.download_button(label="DESCARGAR RECORTABLE",
+                    data=PDFbyte,
+                    file_name="RECORTABLE_LURA_IEO.pdf",
+                    mime='application/octet-stream')
+
+st.text('Aquí tienes unas fotos del barco terminado, para que te ayuden en el montaje')
+
+
+# Despliega la extensión para subir los archivos .btl y .cnv
+col1, col2 = st.columns(2,gap="small")
+
+with col1:
+    imagen_pagina = Image.open(logo_IEO_principal) 
+    st.image(imagen_pagina)   
+    st.caption('Vista general del barco terminado')
+    
+    imagen_pagina = Image.open(logo_IEO_principal) 
+    st.image(imagen_pagina)   
+    st.caption('Detalle de la zona de popa')    
+    
+with col2:
+    imagen_pagina = Image.open(logo_IEO_principal) 
+    st.image(imagen_pagina)   
+    st.caption('Vista general del barco terminado')
+    
+    imagen_pagina = Image.open(logo_IEO_principal) 
+    st.image(imagen_pagina)   
+    st.caption('Detalle de la zona de proa')    
+     
