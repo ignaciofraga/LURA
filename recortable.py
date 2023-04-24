97a9c691474af65c523122f445041f1d9072801a
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2023-04-24 12:33:08 +0200

    yy

--- recortable.py
+++ recortable.py
@@ -8,18 +8,32 @@ Created on Mon Apr 24 11:13:13 2023
 import streamlit as st
 from PIL import Image
 
-logo_IEO_principal   = 'DATOS/IMAGENES/ieo.jpg'    
+logo_IEO_principal   = 'DATOS/IMAGENES/logo-CSIC.jpg' 
+logo_centro_coruna   = 'DATOS/IMAGENES/logo_IEO_Coru.jpg' 
+
+foto_1               = 'DATOS/IMAGENES/ieo.jpg'    
 archivo_recortable   = 'DATOS/PLANO_LURA_A3.pdf'
 
 
-#st.title("**Monta el barco del Centro Oceanográfico en tu propia casa!**")
+# LOGOS INICIO
+col1, col2 = st.columns(2,gap="small")
 
+with col1:
+    imagen_pagina = Image.open(logo_centro_coruna) 
+    st.image(imagen_pagina)     
+    
+with col2:
+    imagen_pagina = Image.open(logo_IEO_principal) 
+    st.image(imagen_pagina)   
+
+
+# TITULO PRINCIPAL
 titulo_principal = '<p style="text-align: center;font-family:Gothic Bold; font-size: 35px;">Monta el barco del Oceanográfico</p>'
 st.markdown(titulo_principal, unsafe_allow_html=True)
 titulo_principal = '<p style="text-align: center;font-family:Gothic Light; font-size: 35px;"> de A Coruña en tu propia casa !!</p>'
 st.markdown(titulo_principal, unsafe_allow_html=True)
 
-
+# DESCARGA RECORTABLE
 titulo_secundario = '<p style="text-align: center;font-family:Gothic Light; font-size: 20px;"> Imprime los planos en un A3, monta el barco y comienza a navegar</p>'
 st.markdown(titulo_secundario, unsafe_allow_html=True)
     
@@ -37,6 +51,7 @@ with col3:
 
 
 
+# FOTOS GUIA
 titulo_fotos = '<p style="text-align: center;font-family:Gothic; font-size: 20px;"> Aquí tienes unas fotos del barco terminado, para que te ayuden en el montaje</p>'
 st.markdown(titulo_fotos, unsafe_allow_html=True)
 
@@ -45,20 +60,20 @@ st.markdown(titulo_fotos, unsafe_allow_html=True)
 col1, col2 = st.columns(2,gap="small")
 
 with col1:
-    imagen_pagina = Image.open(logo_IEO_principal) 
+    imagen_pagina = Image.open(foto_1) 
     st.image(imagen_pagina)   
     st.caption('Vista general del barco terminado')
     
-    imagen_pagina = Image.open(logo_IEO_principal) 
+    imagen_pagina = Image.open(foto_1) 
     st.image(imagen_pagina)   
     st.caption('Detalle de la zona de popa')    
     
 with col2:
-    imagen_pagina = Image.open(logo_IEO_principal) 
+    imagen_pagina = Image.open(foto_1) 
     st.image(imagen_pagina)   
     st.caption('Vista general del barco terminado')
     
-    imagen_pagina = Image.open(logo_IEO_principal) 
+    imagen_pagina = Image.open(foto_1) 
     st.image(imagen_pagina)   
     st.caption('Detalle de la zona de proa')    
      
