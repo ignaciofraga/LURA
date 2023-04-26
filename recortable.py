e6925fd867a7ec558b7953a8f69738fce9198f16
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2023-04-26 15:36:15 +0200

    tt

--- recortable.py
+++ recortable.py
@@ -10,6 +10,7 @@ from PIL import Image
 
 logo_IEO_principal   = 'DATOS/IMAGENES/LOGOS.jpg' 
 logo_centro_coruna   = 'DATOS/IMAGENES/logo_IEO_Coru.jpg' 
+foto_Lura_1          = 'DATOS/IMAGENES/LURA.jpg'
 
 foto_1               = 'DATOS/IMAGENES/ieo.jpg'    
 archivo_recortable   = 'DATOS/PLANO_LURA_A3.pdf'
@@ -34,10 +35,27 @@ tipo_entrada = st.sidebar.radio("Explora!",acciones)
 if tipo_entrada == acciones[0]:
 
     # Texto
-    texto = 'Qué es el Lura'
+    texto = 'Qué es el Lura?'
     titulo_principal = '<p style="text-align: center;font-family:Bahnschrift; font-size: 35px;">' + texto + '</p>'
     st.markdown(titulo_principal, unsafe_allow_html=True)
 
+    # Texto
+    texto = 'El Lura es el barco que utilizan los investigadores del Centro Oceanográfico de A Coruña para estudiar la costa cercana a la ciudad. Se trata de un antiguo buque de pesca, de 14 metros de eslora (longitud) y 4 de manga (anchura) acondicionado para trabajos científicos'
+    titulo_principal = '<p style="text-align: center;font-family:Bahnschrift; font-size: 35px;">' + texto + '</p>'
+    st.markdown(titulo_principal, unsafe_allow_html=True)
+    
+    imagen_pagina = Image.open(foto_Lura_1) 
+    st.image(imagen_pagina) 
+    
+    texto = 'Cuando el tiempo lo permite, los investigadores y marineros del IEO muestrean la columna de agua en una serie de puntos concretos. Se miden diferentes variables físicas (temperatura, salinidad,...), químicas (oxígeno disuelto, pH,...) y biológicas (biomasa de plancton, concentración de clorofila). El IEO lleva casi 40 años haciendo estos estudios, lo que le ha permitido recopilar mucha información, fundamental para estudiar el cambio climático o conocer la salud de nuestra costa.' 
+    titulo_principal = '<p style="text-align: center;font-family:Bahnschrift; font-size: 35px;">' + texto + '</p>'
+    st.markdown(titulo_principal, unsafe_allow_html=True)
+
+    texto = 'El IEO también estudia los bancos de peces de la zona, una información muy útil en una región como Galicia, donde la pesca es una actividad muy importante'
+    titulo_principal = '<p style="text-align: center;font-family:Bahnschrift; font-size: 35px;">' + texto + '</p>'
+    st.markdown(titulo_principal, unsafe_allow_html=True)
+
+'Esta información es la base de estudios como los relacionados con el cambio climático'
 
 if tipo_entrada == acciones[1]:
 
