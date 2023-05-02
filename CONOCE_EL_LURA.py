eb7783f688b367e99b7c97f0c91e71ba5cac3e70
Ignacio Fraga <ignacio.fraga@ieo.csic.es>
2023-05-02 09:34:38 +0200

    Update CONOCE_EL_LURA.py

--- CONOCE_EL_LURA.py
+++ CONOCE_EL_LURA.py
@@ -12,7 +12,7 @@ logo_IEO_principal   = 'DATOS/IMAGENES/LOGOS.jpg'
 logo_centro_coruna   = 'DATOS/IMAGENES/logo_IEO_Coru.jpg' 
 
 foto_Lura_1          = 'DATOS/IMAGENES/LURA.jpg'
-foto_roseta          = 'DATOS/IMAGENES/roseta_red.jpg'
+foto_roseta          = 'DATOS/IMAGENES/roseta.jpg'
 foto_red_plancton    = 'DATOS/IMAGENES/red_plancton.PNG'
 
 logo_IEO_reducido    = 'DATOS/IMAGENES/ieo.ico'
